cd369d6f5f81dba1ec49355e1aadc1b8cc9ab629
Markus Korn <thekorn@gmx.de>
2011-08-19 09:11:43 +0200

    Fixed testcase for escaped chars, it was just a problem of single vs. double quotes.

--- tests/test_search.rb
+++ tests/test_search.rb
@@ -138,22 +138,17 @@ class TestSearch < Test::Unit::TestCase
   end
   
   def test_check_escaped_chars
-    # it is not obvious to me how escaping works here, and how it works within chef
-    # Fact 1: the escaped queries are similar to the ones in the chef wiki
-    # Fact 2: the parser fails to parse this expressions
-    # Fact 3: "a:a::a" == "a:a\:\:a"
-    #   so the way escaping values is described in the wiki looks strange.
     nodes = search(:users, 'tag:tag\:\:test')
     assert nodes.length == 1
-    nodes = search(:users, 'tag:tag::test')
+    nodes = search(:users, "tag:tag\\:\\:test")
     assert nodes.length == 1
     nodes = search(:users, 'tags:tag\:\:first')
     assert nodes.length == 1
-    nodes = search(:users, 'tags:tag::first')
+    nodes = search(:users, "tags:tag\\:\\:first")
     assert nodes.length == 1
     nodes = search(:users, 'tags:tag\:\:*')
     assert nodes.length == 1
-    nodes = search(:users, 'tags:tag::*')
+    nodes = search(:users, "tags:tag\\:\\:*")
     assert nodes.length == 1
   end
   
